6ec017d9043c30e285aed969c9475b607b26b246
leesea <leetpy2@gmail.com>
2018-11-21 10:59:21 -0500

    update structure

--- certificate/init.sh
+++ certificate/init.sh
@@ -5,20 +5,19 @@ workdir=$(cd "$(dirname "$0")";pwd)
 function install_cfssl {
     local install_dir="/usr/local/bin"
     if [ ! -f $install_dir/cfssl ]; then
-        cp -a cfssl_linux-amd64 $install_dir/cfssl
+        cp -a $workdir/bin/cfssl_linux-amd64 $install_dir/cfssl
         chmod a+x $install_dir/cfssl
     fi
 
     if [ ! -f $install_dir/cfssljson ]; then
-        cp -a cfssljson_linux-amd64 $install_dir/cfssljson
+        cp -a $workdir/bin/cfssljson_linux-amd64 $install_dir/cfssljson
         chmod a+x $install_dir/cfssljson
     fi
 
     if [ ! -f $install_dir/cfssl-certinfo ]; then
-        cp -a cfssl-certinfo_linux-amd64 $install_dir/cfssl-certinfo
+        cp -a $workdir/bin/cfssl-certinfo_linux-amd64 $install_dir/cfssl-certinfo
         chmod a+x $install_dir/cfssl-certinfo
     fi
-
 }
 
 function gencert {
